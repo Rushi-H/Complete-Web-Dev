541409f1d7d697baba6e9b2a7060ffc5fa240ca7
Rushi-h <rushihonkande@gmail.com>
2026-03-13 18:53:29 +0530

    jquery started

---
+++ jquery/script.js
@@ -0,0 +1,13 @@
+console.log($("h1").css("font-size"));
+
+
+
+$("h1").css("color","green");
+
+
+$("h1").addClass("big-title")
+
+$("h1").addClass("margin")
+
+// $("h1").removeClass("big-title") 
+$("button").text("Dont Touch");
