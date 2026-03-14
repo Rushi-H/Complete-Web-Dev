8e9bd030891bbfaf623efcdcafa8843e7cda6d21
Rushi-h <rushihonkande@gmail.com>
2026-03-14 16:22:46 +0530

    jquery updated

--- jquery/script.js
+++ jquery/script.js
@@ -17,3 +17,25 @@ $("a").attr("href","https://www.bing.com/")
 $("h1").click(function(){
     $("h1").css("color","purple");
 });
+
+
+$("button").click(function(){
+    $("h1").css("color","red")
+});
+
+
+$(document).keydown(function(event){
+$("h1").text(event.key);
+}
+)
+
+
+// $("h1").on("click", function(){
+//     $("h1").css("color","black")
+// })
+// $("h1").on("click", function(){
+//     $("h1").hide()
+// });
+$("button").on("click", function(){
+    $("h1").animate({opacity: 0}).slideUp().slideDown().animate({opacity: 1})
+})
